6558e14191bf988661fd3e5add6476f107eb99d1
Anilople <Anilople@outlook.com>
2018-11-09 14:09:26 +0800

    erase other disk IO, just keep LBA read and write

--- drivers/disk.h
+++ drivers/disk.h
@@ -1,10 +1,2 @@
-void CHS_read_one_disk_sector(
-    unsigned short *addressBegin, 
-    unsigned short cylinder,
-    unsigned char head, 
-    unsigned char sector,
-    unsigned char sectorCount
-    );
-void LBA_read_one_disk_sector(unsigned short *addressBegin, int LBAstart);
 void LBA_read_disk_sector(unsigned short *addressBegin, int start, unsigned char sectorNumbers);
 void LBA_write_disk_sector(unsigned short *addressBegin, int start, unsigned char sectorNumbers);
