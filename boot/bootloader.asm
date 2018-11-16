3740b158fed95f46239381e77b35a7e60dfd845f
Anilople <Anilople@outlook.com>
2018-11-16 23:20:58 +0800

    in bootloader, change to read 50 sectors.

--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -8,13 +8,14 @@ int 0x15 ; 返回AX, 1KB为单位, AX * 1K 代表在1MB之上可用的内存数�
 mov [0x500], ax ; // 存入0x500这个地址, 给内核使用
 
 ; 将内核加载
-; 15个扇区, 从0x1000开始
+; 从0x1000开始放置数据
+readSectorNumbers equ 50 ; 要读取的扇区个数
 mov ah, 0x02
 mov dl, 0x80 ; 从硬盘用0x80, 软盘采用0
 mov ch, 0
 mov dh, 0
 mov cl, 2 ; 从第2个扇区开始读, 扇区的编号是从1开始的, 不是从0开始
-mov al, 15 ; 15个扇区
+mov al, readSectorNumbers ; readSectorNumbers 个扇区
 ; 设置[es:bx]指向0x1000
 mov bx, 0
 mov es, bx
